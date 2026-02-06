ee0ecdd3d7cfc4e54e21c3dc558ad958d06770aa
PRB-555 <bharanipr24bcg008@skasc.ac.in>
2026-02-06 10:08:43 +0530

    Create MyTest.java

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyTest {
+    @Test
+    public void testMath() {
+        assertEquals("Math should work", 2, 1 + 1);
+    }
+}
